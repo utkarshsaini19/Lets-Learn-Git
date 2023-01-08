e7bc4110b0e7ca3d3f993ffba8f244f90bb54a7d
Utkarsh Saini <utkarshsaini19@gmail.com>
2023-01-08 15:32:39 +0530

    adding Modified Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ package LetsLearnGit;
 public class Sum
 {
     public static void main(String[] args) {
-        System.out.println("The sum is :"+23);
+        System.out.println("The sum is :"+24);
     }
 }
